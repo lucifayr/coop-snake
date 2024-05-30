87aa53c9fff7b600bdeb109f64695596acb1d7cd
lucifayr <lucygschwantner@gmail.com>
2024-05-30 15:57:17 +0200

    fix: update expected enum variant in test

--- coop-snake-app/src/binary/gameBinaryMessage.test.ts
+++ coop-snake-app/src/binary/gameBinaryMessage.test.ts
@@ -20,7 +20,7 @@ describe("GameBinaryMessage", () => {
         const out = binMsgFromBytes(case1);
 
         expect(out.version).toEqual(1);
-        expect(out.messageType).toEqual("PlayerInput");
+        expect(out.messageType).toEqual("PlayerSwipeInput");
         expect(out.dataLength).toEqual(8);
         expect(out.data.buffer).toEqual(
             Uint8Array.of(...[127, 0, 0, 1, 42, 65, 10, 24]).buffer,
